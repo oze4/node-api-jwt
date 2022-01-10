ab597542e3dda5b439862b31e41226e8a4d6715e
Matt Oestreich <matthewpoestreich@gmail.com>
2022-01-09 19:03:45 -0600

    Update connection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 
 if (!process.env.MONGO_URI) {
   console.error("missing env var MONGO_URI");
+  process.exit(1);
 }
 
 const db = mongoose.createConnection(process.env.MONGO_URI, { useNewUrlParser: true });
